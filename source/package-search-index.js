5e846ba871019f7fb5d0bc9941e38118cbffb29c
しろしゅん <39247022+Siroshun09@users.noreply.github.com>
2025-03-29 13:33:48 +0900

    Delete source directory

--- source/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dev.siroshun.jfun.function"},{"l":"dev.siroshun.jfun.result"},{"l":"dev.siroshun.jfun.result.assertion"}];updateSearchResults();
